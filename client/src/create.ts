8ac92e60c3a5719f72944fcd7bea408f91f10d7e
Sidonie <sidonie.mnd@gmail.com>
2025-12-12 01:38:13 +0100

    layout stuff again

--- client/src/create.ts
+++ client/src/create.ts
@@ -13,19 +13,21 @@ document.addEventListener("DOMContentLoaded", () => {
     TurboIcon.config.defaultDirectory = "assets";
 
     if(! sessionStorage.getItem("userId")) {window.location.replace("/login");}
-    if(sessionStorage.getItem("currentPage")) {
-        let createPageLayout = div({classes:"create-page-layout", parent:document.body});
 
-        //page in drawing form
-        let pageDrawing = page({...JSON.parse(sessionStorage.getItem("currentPage")), ...{parent:createPageLayout, classes:"page-drawing"}});
+    let createPageLayout = div({classes:"create-page-layout", parent:document.body});
 
-        //page in list form
-        let pageList = page({...JSON.parse(sessionStorage.getItem("currentPage")), ...{parent:createPageLayout, classes:"page-list"}}, true);
+    //todo tool bar
+    let toolBar = div({parent: createPageLayout, classes:"toolbar"});
+
+    navBar({parent: createPageLayout, classes:"nav-bar"});
+
+    //page in drawing form
+    let pageDrawing = page({...JSON.parse(sessionStorage.getItem("currentPage")), ...{parent:createPageLayout, classes:"page-drawing"}});
+
+    //page in list form
+    let pageList = page({...JSON.parse(sessionStorage.getItem("currentPage")), ...{parent:createPageLayout, classes:"page-list"}}, true);
 
 
-        //todo tool bar
 
 
-    }
-    navBar({parent: document.body});
 });
